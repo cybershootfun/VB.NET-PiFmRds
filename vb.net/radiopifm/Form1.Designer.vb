3d9f853081d7c466c8601cfc9d9b003a9e9fae5b
cybershoot <49350716+cybershootfun@users.noreply.github.com>
2024-05-21 01:52:33 +0300

    Add files via upload

---
+++ vb.net/radiopifm/Form1.Designer.vb
@@ -0,0 +1,345 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 31)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(132, 139)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Ping Rasberrypi"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 324)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(817, 315)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(360, 308)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Status:NotConnected"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(191, 31)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(132, 139)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Connect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(350, 31)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(132, 139)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "Start"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(519, 31)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(132, 139)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Stop"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(681, 31)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(132, 139)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Disconnect"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(191, 223)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(460, 20)
+        Me.TextBox1.TabIndex = 7
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(191, 249)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(460, 43)
+        Me.Button6.TabIndex = 8
+        Me.Button6.Text = "SCP"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(392, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Temp"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 2500
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1200
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.Color.Red
+        Me.Label3.Location = New System.Drawing.Point(360, 295)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(69, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Playing:False"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(241, 4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(132, 22)
+        Me.Button7.TabIndex = 11
+        Me.Button7.Text = "Temp Start"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(454, 4)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(132, 22)
+        Me.Button8.TabIndex = 12
+        Me.Button8.Text = "Temp Stop"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(45, 176)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {9999, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 13
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1234, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(58, 199)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "PI 4 digit code"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(203, 199)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(107, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "PS 8-character name"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(194, 176)
+        Me.TextBox2.MaxLength = 8
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(129, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(350, 176)
+        Me.TextBox3.MaxLength = 0
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(129, 20)
+        Me.TextBox3.TabIndex = 18
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(376, 199)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "RT settings"
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(496, 176)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(155, 23)
+        Me.Button9.TabIndex = 19
+        Me.Button9.Text = "Pi Shutdown"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(674, 176)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(155, 23)
+        Me.Button10.TabIndex = 20
+        Me.Button10.Text = "Pi Reboot"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(67, 295)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 13)
+        Me.Label7.TabIndex = 22
+        Me.Label7.Text = "frequency"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(33, 223)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(140, 20)
+        Me.TextBox4.TabIndex = 23
+        Me.TextBox4.Text = "108"
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.Location = New System.Drawing.Point(25, 247)
+        Me.TrackBar1.Maximum = 108
+        Me.TrackBar1.Minimum = 87
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(140, 45)
+        Me.TrackBar1.TabIndex = 24
+        Me.TrackBar1.Value = 108
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.ClientSize = New System.Drawing.Size(841, 651)
+        Me.Controls.Add(Me.TrackBar1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TrackBar1 As TrackBar
+End Class
---
+++ vb.net/radiopifm/Form1.vb
@@ -0,0 +1,202 @@
+﻿Imports System.Net
+Imports System.IO
+Imports Renci.SshNet
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel
+
+Public Class Form1
+    Private sshClient As SshClient
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim psi As New ProcessStartInfo()
+        psi.FileName = "cmd.exe"
+        psi.RedirectStandardInput = True
+        psi.RedirectStandardOutput = True
+        psi.UseShellExecute = False
+        psi.CreateNoWindow = True
+        Dim process As Process = New Process()
+        process.StartInfo = psi
+        process.Start()
+        process.StandardInput.WriteLine("ping raspberrypi.local")
+        process.StandardInput.Close()
+        Dim output As String = process.StandardOutput.ReadToEnd()
+        AppendTextToRichTextBox(output)
+        RichTextBox1.SelectionStart = RichTextBox1.TextLength
+        RichTextBox1.ScrollToCaret()
+        process.Close()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Dim host As String = "raspberrypi.local"
+        Dim username As String = "username"
+        Dim password As String = "password"
+        sshClient = New SshClient(host, username, password)
+        Try
+            sshClient.Connect()
+            ExecuteCommand(sshClient, "sudo killall pi_fm_rds")
+            ExecuteCommand(sshClient, "sudo killall python3")
+            AppendTextToRichTextBox("Connected to Raspberry Pi at:" & host)
+        Catch ex As Exception
+            AppendTextToRichTextBox("error:" & ex.ToString)
+        End Try
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Try
+            Dim url As String = "http://raspberrypi.local:5000/temperature"
+            Dim request As HttpWebRequest = WebRequest.Create(url)
+            Dim response As HttpWebResponse = request.GetResponse()
+            Dim reader As New StreamReader(response.GetResponseStream())
+            Dim temperature As String = reader.ReadToEnd()
+            response.Close()
+            Label2.Text = temperature
+        Catch ex As Exception
+        End Try
+    End Sub
+    Private Function ExecuteCommand(client As SshClient, commandText As String)
+        Dim command As SshCommand = client.CreateCommand(commandText)
+        command.BeginExecute()
+    End Function
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            Try
+                Dim commandResult As String = ExecuteCommand(sshClient, $"cd PiFmRds/src && sudo ./pi_fm_rds -freq {TextBox4.Text} -audio play.wav -pi {NumericUpDown1.Value} -rt ""{TextBox3.Text}"" -ps ""{TextBox2.Text}""")
+                AppendTextToRichTextBox("at:" & TimeOfDay.Now & " started to play.")
+                Label3.Text = "Playing:True"
+                Label3.ForeColor = Color.Green
+            Catch ex As Exception
+            End Try
+        Else
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            sshClient.Disconnect()
+            RichTextBox1.AppendText("Disconnected From Raspberrypi" & vbNewLine)
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            Label1.Text = "Status:Connected"
+            Label1.ForeColor = Color.Green
+            Label1.Refresh()
+        Else
+            Label1.Text = "Status:NotConnected"
+            Label1.ForeColor = Color.Red
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            Try
+                ExecuteCommand(sshClient, "sudo killall pi_fm_rds")
+                Label3.Text = "Playing:False"
+                AppendTextToRichTextBox("at:" & TimeOfDay.Now & " Stoped Playing")
+                Label3.ForeColor = Color.Red
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            Try
+                ExecuteCommand(sshClient, "cd PiFmRds/src && python3 tem.py")
+                Timer1.Start()
+                AppendTextToRichTextBox("at:" & TimeOfDay.Now & " Temp is on")
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        Timer1.Stop()
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            Try
+                ExecuteCommand(sshClient, "sudo killall python3")
+                AppendTextToRichTextBox("at:" & TimeOfDay.Now & " Temp is off")
+            Catch ex As Exception
+            End Try
+        End If
+    End Sub
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        TextBox4.Text = TrackBar1.Value
+    End Sub
+    Private Sub TextBox1_Click(sender As Object, e As EventArgs) Handles TextBox1.Click
+        Dim openFileDialog1 As New OpenFileDialog()
+        openFileDialog1.Title = "Select WAV File"
+        openFileDialog1.Filter = "WAV files (*.wav)|*.wav" ' Filter to show only WAV files
+        openFileDialog1.RestoreDirectory = True
+        If openFileDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = openFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If File.Exists(TextBox1.Text) Then
+            If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+                ExecuteCommand(sshClient, "sudo killall pi_fm_rds")
+                ExecuteCommand(sshClient, "cd PiFmRds/src && rm play.wav")
+                Dim host As String = "raspberrypi.local"
+                Dim port As Integer = 22
+                Dim username As String = "username"
+                Dim password As String = "password"
+                Dim localFilePath As String = TextBox1.Text
+                Dim remoteFilePath As String = "/home/username/PiFmRds/src/play.wav"
+                UploadFileUsingSftp(host, port, username, password, localFilePath, remoteFilePath)
+            Else
+            End If
+        End If
+    End Sub
+    Public Sub UploadFileUsingSftp(host As String, port As Integer, username As String, password As String, localFilePath As String, remoteFilePath As String)
+        Try
+            AppendTextToRichTextBox("Connecting to " & host & " as " & username)
+            AppendTextToRichTextBox("Local File Path: " & localFilePath)
+            AppendTextToRichTextBox("Remote File Path: " & remoteFilePath)
+            Using client As New SftpClient(host, port, username, password)
+                client.Connect()
+                If client.IsConnected Then
+                    AppendTextToRichTextBox("Connected to " & host)
+                Else
+                    AppendTextToRichTextBox("Failed to connect to " & host)
+                    Exit Sub
+                End If
+                If Not File.Exists(localFilePath) Then
+                    AppendTextToRichTextBox("Local file does not exist: " & localFilePath)
+                    Exit Sub
+                End If
+                Using fileStream As New FileStream(localFilePath, FileMode.Open)
+                    client.UploadFile(fileStream, remoteFilePath)
+                End Using
+                client.Disconnect()
+            End Using
+            AppendTextToRichTextBox("File transferred successfully!")
+            ExecuteCommand(sshClient, $"cd PiFmRds/src && sudo ./pi_fm_rds -freq {TextBox4.Text} -audio play.wav -pi {NumericUpDown1.Value} -rt ""{TextBox3.Text}"" -ps ""{TextBox2.Text}""")
+            Label3.Text = "Playing:True"
+            Label3.ForeColor = Color.Green
+        Catch ex As Exception
+            AppendTextToRichTextBox("Error: " & ex.Message)
+        End Try
+    End Sub
+    Private Sub AppendTextToRichTextBox(text As String)
+        If RichTextBox1.InvokeRequired Then
+            RichTextBox1.Invoke(New Action(Of String)(AddressOf AppendTextToRichTextBox), text)
+        Else
+            RichTextBox1.AppendText(text & Environment.NewLine)
+            RichTextBox1.ScrollToCaret()
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            ExecuteCommand(sshClient, "sudo shutdown")
+            AppendTextToRichTextBox("Shutdowned at " & TimeOfDay.Now)
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If sshClient IsNot Nothing AndAlso sshClient.IsConnected Then
+            ExecuteCommand(sshClient, "sudo reboot")
+            AppendTextToRichTextBox("Rebooted at " & TimeOfDay.Now)
+        End If
+    End Sub
+End Class
